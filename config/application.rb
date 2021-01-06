b500b4e93511abf3f026371b37125f2795eec06f
uchiii1021 <alchemist.licht1010@gmail.com>
2021-01-06 19:14:03 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module PhotoApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_mailer.default_options = { from: 'noreply@mycustomdomain.com' }
   end
 end
