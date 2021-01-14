32f556f143848c122f9ed30774c3ba91c6a95b43
uchiii1021 <alchemist.licht1010@gmail.com>
2021-01-15 04:15:06 +0900

    DB変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'shakatu0105.herokuapp.com', protocol: 'https'}
+  config.action_mailer.default_url_options = { :host => 'shakatu0115.herokuapp.com', protocol: 'https'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
