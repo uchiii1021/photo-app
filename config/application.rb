b6291f37bc75298f78a64fbc93fa1554aafb13f7
uchiii1021 <alchemist.licht1010@gmail.com>
2021-01-14 20:59:22 +0900

    DB変更

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module PhotoApp
     
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
